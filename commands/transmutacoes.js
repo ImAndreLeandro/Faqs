dc9e4640b8bcde96de7ef1c5adbe9cc3fe50821f
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-14 12:23:23 +0100

    Update transmutacoes.js

--- commands/transmutacoes.js
+++ commands/transmutacoes.js
@@ -7,5 +7,15 @@ exports.run = async(client, message, args) => {
     .setDescription("Veja as transmutações, usando `!transmutações <tópico>`"
                    + "\n\n__**Tópicos:**__"
                    + "\n- Escudo(s)\n- Pulseira(s)\n- Colar(es)\n- Sapato(s)\n- Elmo(s)\n- Arma(s)\n- Armadura(s)");
-  message.channel.send(embed);
+  if (args.length === 0) return message.channel.send(embed);
+  if (args.length === 1) {
+    if (args[0].toLowerCase() == 'escudo' || args[0].toLowerCase() == 'escudos') {
+      const emb = new Discord.RichEmbed()
+        .setAuthor("Transmutações")
+        .setColor([54, 57, 64])
+        .setDescription("__**Escudos:**__"
+                       + "\n\nEscudo Falcão :arrow_right: Escudo Universal"
+                       + "\nEscudo Leão Angular :arrow_right: Escudo Divino");
+    }
+  }
 }
