6321b24dea75bb1f9d1377ca6506879e2e7b740c
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-14 15:05:12 +0100

    Update transmutacoes.js

--- commands/transmutacoes.js
+++ commands/transmutacoes.js
@@ -53,11 +53,38 @@ exports.run = async(client, message, args) => {
       const emb = new Discord.RichEmbed()
         .setAuthor("Transmutações", message.guild.iconURL)
         .setColor([54, 57, 64])
-        .setDescription("__**Escudos:**__"
+        .setDescription("__**Elmos:**__"
                        + "\n\n<:Elmo_Condestavel:467681134986395658> Elmo Condestável :arrow_right: Elmo Pacífico"
                        + "\n<:Capuz_Ara:467681134663434261> Capuz Aranha :arrow_right: Capuz Oricalcus"
                        + "\n<:Chapeu_Das_Alm:467681134910636062> Chapéu das Almas :arrow_right: Elmo da Dor"
                        + "\n<:Elmo_Mag:467681134940127242> Elmo Mágico :arrow_right: Elmo Destemido")
+      .setTimestamp();
+      message.channel.send(emb);
+    }
+    if (args[0].toLowerCase() == 'arma' || args[0].toLowerCase() == 'armas') {
+      const emb = new Discord.RichEmbed()
+        .setAuthor("Transmutações", message.guild.iconURL)
+        .setColor([54, 57, 64])
+        .setDescription("__**Armas:**__"
+                       + "\n\nEspada de Batalha :arrow_right: Espada Sirius :arrow_right: Espada Rúnica :arrow_right: Espada Necro"
+                       + "\nPartidário :arrow_right: Lâmina Solar :arrow_right: Lâmina Necro"
+                       + "\nLâmina Zanna Fantasma :arrow_right: Espada Sagrada :arrow_right: Lâmina Dente do Paraíso :arrow_right: Espada Necro"
+                       + "\nArco Paraíso Amarelo :arrow_right: Arco Elementar :arrow_right: Arco Fénix :arrow_right: Arco Necro"
+                       + "\nLeque da Salvação :arrow_right: Leque do Demónio :arrow_right: Leque do Dragão :arrow_right: Leque Necro"
+                       + "\nFaca Paraíso :arrow_right: Adaga Gemea Lunar :arrow_right: Lâmina 5 Elementos :arrow_right: Adaga Necro"
+                       + "\nCampainha Céu e Terra :arrow_right: Campainha do Dragão :arrow_right: Campainha do Coração :arrow_right: Campainha Necro")
+      .setTimestamp();
+      message.channel.send(emb);
+    }
+    if (args[0].toLowerCase() == 'armadura' || args[0].toLowerCase() == 'armaduras') {
+      const emb = new Discord.RichEmbed()
+        .setAuthor("Transmutações", message.guild.iconURL)
+        .setColor([54, 57, 64])
+        .setDescription("__**Armaduras:**__"
+                       + "\n\nArmadura de Aço Preto :arrow_right: Armadura Dragão Azul :arrow_right: Armadura Diabo :arrow_right: Warrior Necro"
+                       + "\nQuimono Vento Preto :arrow_right: Quimono Dragão Azul :arrow_right: Quimono Lorde Dragão :arrow_right: Ninja Necro"
+                       + "\nArmadura Magia Negra :arrow_right: Armadura Aura Negra :arrow_right: Armadura de Ossos :arrow_right: Sura Necro"
+                       + "\nRobe Preto :arrow_right: Robe do Dragão :arrow_right: Roupas Douradas :arrow_right: Xamã Necro")
       .setTimestamp();
       message.channel.send(emb);
     }
