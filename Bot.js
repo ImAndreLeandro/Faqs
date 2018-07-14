1c8a54ea295297fc3505f565fb8540ff8abb8df1
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-14 12:18:18 +0100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -21,7 +21,6 @@ client.on("message", async message => {
 	if (message.content.indexOf(config.prefix) !== 0) return;
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
-	if (command === "transmutações") command = "transmutacoes";
     
 	  try {
 	    let commands = require(`./commands/${command}.js`);
