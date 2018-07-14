7d8a39a5a6ea2bb26eab59cde1d5392f310dcce3
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-14 10:47:01 +0100

    Update transmutacoes.js

--- commands/transmutacoes.js
+++ commands/transmutacoes.js
@@ -1,5 +1,11 @@
 const Discord = require('discord.js');
 
 exports.run = async(client, message, args) => {
-  message.channel.send(`Test command!`);
+  const embed = Discord.RichEmbed()
+    .setAuthor("Transmutações!")
+    .setColor([54, 57, 64])
+    .setDescription("Veja as transmutações, usando `!transmutações <tópico>`"
+                   + "\n\n__**Tópicos:**__"
+                   + "\n- Escudo(s)\n- Pulseira(s)\n- Colar(es)\n- Sapato(s)\n- Elmo(s)\n- Arma(s)\n- Armadura(s)");
+  message.channel.send(embed);
 }
